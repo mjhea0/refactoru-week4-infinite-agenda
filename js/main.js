a0a664f19c7fe7723f8e127a967e1d8a49940e72
Michael Herman <hermanmu@gmail.com>
2013-10-06 11:22:06 -0600

    updated

--- js/main.js
+++ js/main.js
@@ -1,151 +1,105 @@
 $(document).ready(function (){
 
+	//** globals */
+	var weeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+	var count = 0
+	
 
-	// //Week and Date Rendering
-	// var count = 0
-	// var dateCounter = function() {
-	// 	var currentDate = new Date
-	// 	var newDate = new Date(currentDate);
-	// 	newDate.setDate(newDate.getDate() + count);
-	// 	var nd = new Date(newDate);
-	// 	count++
-	// 	calendarDay = nd.toString()
-	// 	counsle.log(calendarDay)
-	// 	return calendarDay.slice(3,15);
-	// };
-
-
-	//Rendering Initial Week
-	var initialWeek = function (){
-		var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-		
-		var calList = $('<ul class="calendarWeek"></ul>')
-		
-		for(var i=(new Date().getDay()); i<weekDays.length; i++) {
-			var week = createDay(weekDays[i]);
-			var item = $('<li></li>');
-			item.append(week);
-			calList.append(item);
-		}
-
-		$('.calendarList').append(calList);
-
+	//** Calculate week day */
+	function weekDay() {
+		var currentDate = new Date
+		var newDate = new Date(currentDate)
+		newDate.setDate(newDate.getDate() + count)
+		var dayOfWeek = new Date(newDate)
+		count++
+		calendarDay = dayOfWeek.toString()
+		return calendarDay.slice(3,15)
 	};
 
-	//Rendering one week at a time
-	var renderWeek = function (){
-		var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-		
-		var calList = $('<ul class="calendarWeek"></ul>')
-		
-		for(var i=0; i<weekDays.length; i++) {
-			var week = createDay(weekDays[i]);
+	//* Calculate the first week */
+	function firstWeek (weekArray) {
+		var listAgenda = $('<ul class="week"></ul>')
+		for(var i=(new Date().getDay()); i < 7; i++) {
 			var item = $('<li></li>');
-			item.append(week);
-			calList.append(item);
+			listAgenda.append(item);
 		}
-
-		$('.calendarList').append(calList);
-		console.log('renderWeek')
-
+		$('.agenda').append(listAgenda);
 	};
 
+	//** Generate weeks */
+	function allWeeks(weekArray){
+	var listAgenda = $('<ul class="week"></ul>')
+		for(var i=0; i < weeks.length; i++) {
+			var week = createDay(weeks[i]);
+			var item = $('<li></li>');
+			item.append(week);
+			listAgenda.append(item);
+		}
+		$('.agenda').append(listAgenda);
+	};
 
+	//** Append to HTML */
+	function createDay(weeks) {
+		var dayOfWeek = $(
+			'<div class="dayOfWeek">' +
+				'<h4 class="dayHead">' + weeks + ", " + weekDay() + '</h4>' +
+				'<div class="addAppt">' +
+					'<button type="button" class="btn btn-primary btn-xs addApptButton">Add</button>' + " " +
+					'<span>A new freaking appointment</span>' +
+				'</div>' +
+				'</h4>' +
+			'</div>' +
+				'<br>' +
+			'</div>')
+		return dayOfWeek;
+	};
 
-	var createDay = function(weekdays) {
-			var dayOfWeek = $('<div class="dayOfWeek"><h3 class="dayHead text-muted">{0}</h3></div>'.supplant([weekdays]));
-			var date = $('<div class="date row"><h4 class="text-info">'+ dateCounter() +'</h4></div><div class="addAppt"><button type="button" class="btn btn-success addApptButton">+</button><span class="apptAddText">Click to add another appointment</span></div><hr>');
-			var appt = $('<div class="apptContainer"><a href="#"  data-placement="right" class="editable editable-click editable open appt small text-muted">Click to enter appointment</a><div class="delAppt"><button type="button" class="btn btn-xs btn-danger delAppt">&times</button><span class="apptDelText">Click to delete this appointment</span></div></div>');
-			$('.appt').editable({
-			    type: 'text',
-			    pk: 1,
-			    url: '#',
-			    title: 'Add Appointment'
-			});
-
-			
-			dayOfWeek.append(date);
-			dayOfWeek.append(appt);
-
-			return dayOfWeek;
-		};
-
-	// Infinite Scroll
-	var scrollTop = 0;
-	var docHeight; 
-	var winHeight; 
-	var winViewPercent; 
-	var scrollPercent; 
-
-	$(window).scroll(function(){
-		 scrollTop = $(window).scrollTop();
-		 docHeight = $(document).height();
-		 winHeight = $(window).height();
-		 winViewPercent = (1-(docHeight-winHeight)/docHeight)
-		 scrollPercent = ((scrollTop) / (docHeight - winHeight) * 100);
-
-			if(scrollPercent>=95){
-				renderWeek()
-			}
-		
-	});
-
-
-	// run
-	initialWeek();
-	renderWeek();
-	renderWeek();
-
+	//** x editable */
 	$(document).on('click', '.addAppt', function (){
-		$(this).parent().append($('<div class="apptContainer"><a href="#"  data-placement="right" class="editable editable-click editable open appt small text-muted">Click to enter appointment</a><div class="delAppt"><button type="button" class="btn btn-xs btn-danger delAppt">&times</button><span class="apptDelText">Click to delete this appointment</span></div></div>'));
+		$(this).parent().append($(
+			'<div class="delAppt">' +
+				'<h6><button type="button" class="btn btn-xs btn-danger">Delete</button>' + " " +
+				'<a href="#" class="editable editable-click appt">Click to enter appointment</a></h6>' +
+			'</div>'));
 
-				$('.appt').editable({
-			    type: 'text',
-			    pk: 1,
-			    url: '#',
-			    title: 'Add Appointment'
-			});
+		 //editable popup
+		$.fn.editable.defaults.mode = 'popup'; 
+		$('.appt').editable()
 		
 	});
 
-
-	// add appointment button
-	$(document).on('mouseover', '.addAppt', function (){
-		$(this).children('.apptAddText').fadeIn()
-	});
-
-	$(document).on('mouseleave', '.addAppt', function (){
-		$(this).children('.apptAddText').fadeOut()
-	});
-
-	// delete button functionality
+	// delete 
 	$(document).on('click', '.delAppt', function (){
-		$(this).siblings('a').andSelf().fadeOut()
-
+		$(this).unbind('a').andSelf()
 	});
 
-	$(document).on('mouseover', '.delAppt', function (){
-		$(this).children('.apptDelText').fadeIn()
-	});
 
-	$(document).on('mouseleave', '.delAppt', function (){
-		$(this).children('.apptDelText').fadeOut()
-	});
+	//** Infinite Scroll */
+	function infiniteScroll() {
+		var scrollTop = 0;
+		var docHeight; 
+		var winHeight; 
+		var winViewPercent; 
+		var scrollPercent; 
 
-	// helper
+		$(window).scroll(function(){
+			 scrollTop = $(window).scrollTop();
+			 docHeight = $(document).height();
+			 winHeight = $(window).height();
+			 winViewPercent = (1-(docHeight-winHeight)/docHeight)
+			 scrollPercent = ((scrollTop) / (docHeight - winHeight) * 100);
 
-	if (!String.prototype.supplant) {
-	  String.prototype.supplant = function (o) {
-	    return this.replace(
-	      /\{([^{}]*)\}/g,
-	      function (a, b) {
-	        var r = o[b];
-	        return typeof r === 'string' || typeof r === 'number' ? r : a;
-	      }
-	    );
-	  };
+				if(scrollPercent>=95){
+					allWeeks()
+				}
+		});
+	}
 
 
+	//** run */
+	firstWeek(weeks);
+	allWeeks(weeks);
+	infiniteScroll()
 
 
 });
