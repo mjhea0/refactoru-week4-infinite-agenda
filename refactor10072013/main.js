35385a46372aa714eeec9d51b1fc6179ddab1b41
Michael Herman <hermanmu@gmail.com>
2013-10-07 07:35:58 -0600

    another refactor

--- refactor10072013/main.js
+++ refactor10072013/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	// Globals
 	var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 	var weekday = 0;
-	var count = 0
+	var count = 1
 
 	function createDay() {
 		var currentDate = new Date
@@ -71,9 +71,10 @@ $(document).ready(function(){
 				'<tbody>' +
 				'<tr>' +
 				'<td><div class="col-md-4">' + incrementDay + '</div></td>' +
-				'<td><div class="col-md-8 appointment">Click to enter an appointment</div></td>'
+				'<td><div class="col-md-4">' + count + '</div></td>' +
+				'<td><div class="col-md-8 appointment">Click to enter an appointment</div></td></tr>'
 			)
-
+			count++
 			$('.table').append(newDay);
 
 			$(".appointment").click(editApt);
